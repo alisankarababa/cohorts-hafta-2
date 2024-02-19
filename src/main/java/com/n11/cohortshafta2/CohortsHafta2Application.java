ebe205ddd241cf473fdb054e3a8357789f8c06c8
Alişan <alisankarababa@gmail.com>
2024-02-19 23:40:57 +0300

    first commit

---
+++ src/main/java/com/n11/cohortshafta2/CohortsHafta2Application.java
@@ -0,0 +1,13 @@
+package com.n11.cohortshafta2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohortsHafta2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohortsHafta2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/n11/cohortshafta2/CohortsHafta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n11.cohortshafta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohortsHafta2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
